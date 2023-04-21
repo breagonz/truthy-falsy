8905af2261523f1531c0243c00642c04773b3b02
Breanna <b.gonzales21@yahoo.com>
2023-04-20 18:38:06 -0700

    added info

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function checkValue() {
 	// Get the input value
 	var value = document.getElementById("input").value;
-	
+	let isFalsy = value == "false" || value === "" || value == "null" || value == "NaN" || value == 0 || value == "undefined"
 	// TODO: Check if the value is truthy or falsy
-	if ( value == "false" || value === "" || value == "null" || value == "NaN" || value == 0 || value == "undefined"){
+	if ( isFalsy){
 		document.getElementById("result").innerHTML = value + " is falsy!";
         document.getElementById("result").style.color = "red"
     } else {
